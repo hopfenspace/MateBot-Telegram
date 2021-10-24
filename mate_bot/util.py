be1767232cf23bef66a6b9d02d976aeee0f4fc6f
Chris <chris@hopfen.space>
2021-10-24 22:35:54 +0200

    Improved the 'util.safe_send' function

--- mate_bot/util.py
+++ mate_bot/util.py
@@ -11,7 +11,12 @@ import telegram
 _logger = _logging.getLogger("error")
 
 
-def safe_send(default: Callable[[], Any], fallback: Callable[[], Any], msg: Optional[str] = None) -> bool:
+def safe_send(
+        default: Callable[[], Any],
+        fallback: Callable[[], Any],
+        msg: Optional[str] = None,
+        obj: bool = True,
+) -> Any:
     """
     Safely send rich text messages to the Bot API, catching entity parsing problems
 
@@ -21,16 +26,18 @@ def safe_send(default: Callable[[], Any], fallback: Callable[[], Any], msg: Opti
     :param fallback: callable that doesn't take any args and will only be called when
         sending the message using the default callable failed with an entity parser error
     :param msg: optional message that was attempted to be sent, for inclusion in logs
+    :param obj: switch to enable returning the result of the function (otherwise, determines
+        the executed function using True for default or False for fallback instead)
     :return: whether the normal (default) message was delivered, or the fallback
     """
 
     try:
-        default()
-        return True
+        default_obj = default()
+        return default_obj if obj else True
     except telegram.error.BadRequest as exc:
         if not str(exc).startswith("Can't parse entities"):
             raise
         _logger.error(f"Sending {msg!r} failed due to entity parsing problems: {exc!s}")
-        fallback()
+        fallback_obj = fallback()
         _logger.debug("The message has been sent without formatting enabled (raw text).")
-        return False
+        return fallback_obj if obj else False

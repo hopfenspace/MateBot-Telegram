cda41d4b72343ddf2a724756309291b270e9228c
Chris <chris@hopfen.space>
2021-12-29 23:16:00 +0100

    Added the error module

---
+++ matebot_telegram/err.py
@@ -0,0 +1,21 @@
+"""
+MateBot telegram exception classes
+"""
+
+
+class MateBotException(Exception):
+    """
+    Base class for all project-wide exceptions
+    """
+
+
+class ParsingError(MateBotException):
+    """
+    Exception raised when the argument parser throws an error
+
+    This is likely to happen when a user messes up the syntax of a
+    particular command. Instead of exiting the program, this exception
+    will be raised. You may use it's string representation to gain
+    additional information about what went wrong. This allows a user
+    to correct its command, in case this caused the parser to fail.
+    """

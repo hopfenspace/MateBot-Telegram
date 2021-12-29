b1aaae5f53e5f244b6c14b933c5de3534a3bce62
Chris <chris@hopfen.space>
2021-12-29 03:57:14 +0100

    Fixed the commands 'zwegat' and 'balance'

--- matebot_telegram/commands/balance.py
+++ matebot_telegram/commands/balance.py
@@ -2,17 +2,13 @@
 MateBot command executor classes for /balance
 """
 
-import logging
-
 import telegram
 
-from mate_bot.state.user import MateBotUser
-from mate_bot.commands.base import BaseCommand
-from mate_bot.parsing.types import user as user_type
-from mate_bot.parsing.util import Namespace
-
-
-logger = logging.getLogger("commands")
+from matebot_telegram import util
+from matebot_telegram.connector import APIConnector
+from matebot_telegram.base import BaseCommand
+from matebot_telegram.parsing.types import user as user_type
+from matebot_telegram.parsing.util import Namespace
 
 
 class BalanceCommand(BaseCommand):
@@ -32,19 +28,19 @@ class BalanceCommand(BaseCommand):
 
         self.parser.add_argument("user", type=user_type, nargs="?")
 
-    def run(self, args: Namespace, update: telegram.Update) -> None:
+    def run(self, args: Namespace, update: telegram.Update, connect: APIConnector) -> None:
         """
         :param args: parsed namespace containing the arguments
         :type args: argparse.Namespace
         :param update: incoming Telegram update
         :type update: telegram.Update
+        :param connect: API connector
+        :type connect: matebot_telegram.connector.APIConnector
         :return: None
         """
 
         if args.user:
-            user = args.user
-            update.effective_message.reply_text(f"Balance of {user.name} is: {user.balance / 100 : .2f}€")
-
+            update.effective_message.reply_text(f"Balance of {args.user.name} is: {args.user.balance / 100 : .2f}€")
         else:
-            user = MateBotUser(update.effective_message.from_user)
-            update.effective_message.reply_text(f"Your balance is: {user.balance / 100 :.2f}€")
+            balance = util.get_user_by_telegram_id(update.effective_message.from_user.id, connect).balance
+            update.effective_message.reply_text(f"Your balance is: {balance / 100 :.2f}€")
--- matebot_telegram/commands/zwegat.py
+++ matebot_telegram/commands/zwegat.py
@@ -6,9 +6,10 @@ import logging
 
 import telegram
 
-from mate_bot.state.user import CommunityUser, MateBotUser
-from mate_bot.commands.base import BaseCommand
-from mate_bot.parsing.util import Namespace
+from matebot_telegram import schemas, util
+from matebot_telegram.base import BaseCommand
+from matebot_telegram.connector import APIConnector
+from matebot_telegram.parsing.util import Namespace
 
 
 logger = logging.getLogger("commands")
@@ -26,20 +27,25 @@ class ZwegatCommand(BaseCommand):
             "This command can only be used by internal users."
         )
 
-    def run(self, args: Namespace, update: telegram.Update) -> None:
+    def run(self, args: Namespace, update: telegram.Update, connect: APIConnector) -> None:
         """
         :param args: parsed namespace containing the arguments
         :type args: argparse.Namespace
         :param update: incoming Telegram update
         :type update: telegram.Update
+        :param connect: API connector
+        :type connect: matebot_telegram.connector.APIConnector
         :return: None
         """
 
-        user = MateBotUser(update.effective_message.from_user)
-        if not self.ensure_permissions(user, 2, update.effective_message):
+        sender = util.get_user_by_telegram_id(update.effective_message.from_user.id, connect)
+        if sender.external or not sender.permission:
+            update.effective_message.reply_text("You don't have permission to perform this command.")
             return
 
-        total = CommunityUser().balance / 100
+        community = schemas.User(**connect.get("/v1/users/community").json())
+
+        total = community.balance / 100
         if total >= 0:
             update.effective_message.reply_text(f"Peter errechnet ein massives Vermögen von {total:.2f}€")
         else:

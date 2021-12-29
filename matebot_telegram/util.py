42358fdedaf9b4adf4971b0c35edaf5af418c8ec
Chris <chris@hopfen.space>
2021-12-29 03:42:55 +0100

    Added the global universal error handler to the utils package

--- matebot_telegram/util.py
+++ matebot_telegram/util.py
@@ -1,9 +1,13 @@
+import sys
+import json
 import logging
+import traceback
 from typing import Any, Callable, Union
 
-import telegram
+import requests
+import telegram.ext
 
-from . import connector, schemas
+from . import config, connector, schemas
 
 
 def safe_send(bot: telegram.Bot, chat_id: Union[int, str], default: str, fallback: str, *args, **kwargs) -> bool:
@@ -20,7 +24,7 @@ def safe_send(bot: telegram.Bot, chat_id: Union[int, str], default: str, fallbac
         return False
 
 
-def safe_call(default: Callable[[], Any], fallback: Callable[[], Any],logger: logging.Logger = None) -> bool:
+def safe_call(default: Callable[[], Any], fallback: Callable[[], Any], logger: logging.Logger = None) -> bool:
     try:
         default()
         return True
@@ -56,3 +60,68 @@ def get_user_by_telegram_id(telegram_id: int, connect: connector.APIConnector = 
 
 def handle_unknown_user():
     raise NotImplementedError
+
+
+def log_error(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    """
+    Log any error and its traceback to sys.stdout and send it to developers
+
+    :param update: Telegram Update where the error probably occurred
+    :type update: telegram.Update
+    :param context: context of the error
+    :type context: telegram.ext.CallbackContext
+    :return: None
+    """
+
+    logger = logging.getLogger("error")
+    if update is None:
+        logger.warning("Error handler called without Update object. Check for network/connection errors!")
+        token = config.config["token"]
+        response = requests.get(f"https://api.telegram.org/bot{token}/getme")
+        if response.status_code != 200:
+            logger.error("Network check failed. Telegram API seems to be unreachable.")
+        else:
+            logger.debug("Network check succeeded. Telegram API seems to be reachable.")
+
+    if not any(sys.exc_info()):
+        logger.error("Error handler called without an exception. Stack trace following as debug message...")
+        logger.debug("".join(traceback.format_stack()))
+        return
+
+    logger.exception("Something raised an unhandled exception, it will be sent to the developers")
+
+    def send_to(env, rcv, text, parse_mode, extra_text=None) -> None:
+        try:
+            msg = env.bot.send_message(rcv, text, parse_mode=parse_mode)
+            if extra_text is not None:
+                msg.reply_text(extra_text, parse_mode=parse_mode, quote=True)
+        except telegram.TelegramError:
+            logger.exception(f"Error while sending logs to {rcv}!")
+
+    for receiver in config.config["chats"]["notification"]:
+        send_to(
+            context,
+            receiver,
+            f"Unhandled exception: {sys.exc_info()[1]}",
+            None
+        )
+
+    for receiver in config.config["chats"]["stacktrace"]:
+        send_to(
+            context,
+            receiver,
+            f"```\n{traceback.format_exc()}```",
+            "MarkdownV2"
+        )
+
+    for receiver in config.config["chats"]["debugging"]:
+        extra = "No Update object found."
+        if update is not None:
+            extra = json.dumps(update.to_dict(), indent=2, sort_keys=True)
+        send_to(
+            context,
+            receiver,
+            f"```\n{traceback.format_exc()}```",
+            "MarkdownV2",
+            f"Extended debug information:\n```\n{extra}```"
+        )

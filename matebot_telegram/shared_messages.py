3778d4921d484ebba141fdafbf48151bc2abe102
Chris <chris@hopfen.space>
2022-01-19 09:31:47 +0100

    Rebuilt the shared message handler using the new bot storage

--- matebot_telegram/shared_messages.py
+++ matebot_telegram/shared_messages.py
@@ -2,16 +2,10 @@
 MateBot telegram shared message library
 """
 
-import os
 import threading
 from typing import Dict, List, Optional, Union
 
-try:
-    import ujson as json
-except ImportError:
-    import json
-
-from .config import config
+from . import bot_storage
 
 
 class SharedMessage:
@@ -35,20 +29,15 @@ class SharedMessage:
         }
 
 
-class SharedMessageHandler:
-    def __init__(self, storage_path: str):
-        self.storage_path = storage_path
+class SharedMessageHandler(bot_storage.SimpleStorageClient):
+    IDENTIFIER = "shared-messages"
+
+    def __init__(self, storage: bot_storage.BotStorage):
+        super(SharedMessageHandler, self).__init__(storage)
         self._lock = threading.Lock()
-        if not os.path.exists(self.storage_path):
-            with self._lock:
-                with open(self.storage_path, "w") as f:
-                    f.write("[]")
 
     def get_all_messages(self) -> List[SharedMessage]:
-        with self._lock:
-            with open(self.storage_path) as f:
-                content = json.load(f)
-        return [SharedMessage(**entry) for entry in content]
+        return [SharedMessage(**msg) for msg in self.get()]
 
     def get_messages_of(self, share_type: str, share_id: Optional[int] = None) -> List[SharedMessage]:
         filtered_msgs = [shared_msg for shared_msg in self.get_all_messages() if shared_msg.share_type == share_type]
@@ -58,46 +47,31 @@ class SharedMessageHandler:
 
     def add_message(self, shared_message: SharedMessage) -> bool:
         with self._lock:
-            with open(self.storage_path) as f:
-                content = json.load(f)
-            content.append(shared_message.to_dict())
-            with open(self.storage_path, "w") as f:
-                json.dump(content, f)
-        return True
+            return self.add(shared_message.to_dict())
 
     def add_message_by(self, share_type: str, share_id: int, chat_id: int, message_id: int) -> bool:
-        return self.add_message(SharedMessage(share_type, share_id, chat_id, message_id))
+        with self._lock:
+            return self.add(SharedMessage(share_type, share_id, chat_id, message_id).to_dict())
 
     def delete_message(self, shared_message: SharedMessage) -> bool:
         with self._lock:
-            with open(self.storage_path) as f:
-                content = json.load(f)
-            old_length = len(content)
-            shared_messages = [
-                msg.to_dict() for msg in [SharedMessage(**entry) for entry in content]
-                if msg != shared_message
-            ]
-            new_length = len(shared_messages)
-            with open(self.storage_path, "w") as f:
-                json.dump(content, f)
-        return old_length > new_length
+            return self.delete(shared_message.to_dict())
 
     def delete_message_by(self, share_type: str, share_id: int, chat_id: int, message_id: int) -> bool:
-        return self.delete_message(SharedMessage(share_type, share_id, chat_id, message_id))
+        with self._lock:
+            return self.delete(SharedMessage(share_type, share_id, chat_id, message_id).to_dict())
 
     def delete_messages(self, share_type: str, share_id: int) -> bool:
         with self._lock:
-            with open(self.storage_path) as f:
-                content = json.load(f)
-            old_length = len(content)
-            content = [
-                msg.to_dict() for msg in [SharedMessage(**entry) for entry in content]
+            old_messages = [SharedMessage(**d) for d in self.get()]
+            new_messages = [
+                msg for msg in old_messages
                 if msg.share_type != share_type or msg.share_id != share_id
             ]
-            new_length = len(content)
-            with open(self.storage_path, "w") as f:
-                json.dump(content, f)
-        return old_length > new_length
+            self.delete_all()
+            for msg in new_messages:
+                self.add(msg.to_dict())
+        return len(old_messages) > len(new_messages)
 
 
-shared_message_handler = SharedMessageHandler(config["bot-storage"])
+shared_message_handler = SharedMessageHandler(bot_storage.storage)

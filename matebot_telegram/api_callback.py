7594996c699a9321b6d3fb1000cfe21c4b7b7e05
Chris <chris@hopfen.space>
2022-02-03 16:33:12 +0100

    Rewrote the callback handler using the base class from the SDK

--- matebot_telegram/api_callback.py
+++ matebot_telegram/api_callback.py
@@ -5,67 +5,50 @@ MateBot API callback handler implementation
 import asyncio
 import inspect
 import logging
-import collections
-from typing import Awaitable, Callable, Dict, List, Optional, Tuple
+from typing import Awaitable, Callable, Optional
 
 import telegram
 import tornado.web
-from matebot_sdk.base import CallbackUpdate
+from matebot_sdk.base import BaseCallbackDispatcher, CallbackUpdate
 
 from . import util
 
 
-logger = logging.getLogger("api-callback")
-
 CALLBACK_TYPE = Callable[[CallbackUpdate, str, int, telegram.Bot, logging.Logger, ...], Optional[Awaitable[None]]]
-STORAGE_TYPE = Dict[
-    Tuple[CallbackUpdate, Optional[str]],
-    List[Tuple[CALLBACK_TYPE, tuple, dict]]
-]
 
 
-class APICallbackDispatcher:
-    def __init__(self):
-        self._storage: STORAGE_TYPE = collections.defaultdict(list)
+class APICallbackDispatcher(BaseCallbackDispatcher):
+    def __init__(self, bot: telegram.Bot):
+        super().__init__(logger=logging.getLogger("api-callback"))
+        self.bot = bot
 
-    def register(self, event: Tuple[CallbackUpdate, Optional[str]], func: CALLBACK_TYPE, *args, **kwargs):
-        self._storage[event].append((func, args, kwargs))
-
-    def dispatch(self, method: CallbackUpdate, model: str, model_id: int, bot: telegram.Bot):
-        for event in self._storage:
-            if event[0] == method and (event[1] is None or event[1] == model):
-                for handler in self._storage.get(event):
-                    func, args, kwargs = handler
-                    try:
-                        result = func(method, model, model_id, bot, logger, *args, **kwargs)
-                        if result is not None:
-                            if not inspect.isawaitable(result):
-                                raise TypeError(
-                                    f"{func} should return Optional[Awaitable[None]], but got {type(result)}"
-                                )
-                            asyncio.run_coroutine_threadsafe(result, loop=util.event_loop).result()
-                    except Exception as exc:
-                        logger.warning(f"{type(exc).__name__} in API callback handler for event {event}")
-                        raise
-
-
-dispatcher = APICallbackDispatcher()
+    def run_callback(self, func: CALLBACK_TYPE, method: CallbackUpdate, model: str, model_id: int, *args, **kwargs):
+        result = func(method, model, model_id, self.bot, self.logger, *args, **kwargs)
+        if result is not None:
+            if not inspect.isawaitable(result):
+                raise TypeError(f"{func} should return Optional[Awaitable[None]], but got {type(result)}")
+            if not util.event_loop:
+                raise RuntimeError(f"Event loop is not defined, can't run coroutine {result}")
+            asyncio.run_coroutine_threadsafe(result, loop=util.event_loop).result()
 
 
 class APICallbackApp(tornado.web.Application):
     def __init__(self, bot: telegram.Bot):
         handlers = [(r"(?i)/(create|update|delete)/(.+)/([0-9]+)", APICallbackHandler, {"bot": bot})]
         tornado.web.Application.__init__(self, handlers)
+        self.logger = logging.getLogger("api-server")
 
     def log_request(self, handler: tornado.web.RequestHandler) -> None:
-        logger.debug(f"Processed callback query request '{handler.request.path}': code {handler.get_status()}")
+        self.logger.debug(f"Processed callback query request '{handler.request.path}': code {handler.get_status()}")
 
 
 class APICallbackHandler(tornado.web.RequestHandler):
-    bot: telegram.Bot
+    logger: logging.Logger
+    dispatcher: APICallbackDispatcher
 
     def initialize(self, bot: telegram.Bot) -> None:
-        self.bot = bot
+        self.logger = logging.getLogger("api-handler")
+        self.dispatcher = APICallbackDispatcher(bot)
 
     def data_received(self, chunk: bytes) -> Optional[Awaitable[None]]:
         raise NotImplementedError
@@ -74,10 +57,10 @@ class APICallbackHandler(tornado.web.RequestHandler):
         action = str(action).lower()
         model = str(model).lower()
         model_id = int(model_id)
-        logger.debug(f"Incoming callback query: '{action.upper()} {model} {model_id}'")
+        self.logger.debug(f"Incoming callback query: '{action.upper()} {model} {model_id}'")
         method = {
             "create": CallbackUpdate.CREATE,
             "update": CallbackUpdate.UPDATE,
             "delete": CallbackUpdate.DELETE
         }[action]
-        dispatcher.dispatch(method, model, model_id, self.bot)
+        self.dispatcher.dispatch(method, model, model_id)

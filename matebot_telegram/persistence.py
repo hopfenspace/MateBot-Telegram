42fcbc8b6155bd29aca43f899a7a71ab7314f3e3
Chris <chris@hopfen.space>
2022-04-28 01:35:19 +0200

    Added models for shared messages and telegram users

--- matebot_telegram/persistence.py
+++ matebot_telegram/persistence.py
@@ -4,10 +4,12 @@ MateBot telegram persistence library
 
 import sys
 from typing import Optional
+from datetime import datetime as _dt
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, DateTime, FetchedValue, Integer, String
 from sqlalchemy.engine import Engine as _Engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from sqlalchemy.sql import func
 
 
 DEFAULT_DATABASE_URL: str = "sqlite:///db.sqlite3"
@@ -78,3 +80,39 @@ def get_new_session() -> Session:
     if _make_session is None or _engine is None:
         init(DEFAULT_DATABASE_URL)
     return _make_session()
+
+
+class TelegramUser(Base):
+    """
+    Model representing one end-user alias bind to a MateBot user
+    """
+
+    __tablename__ = "telegram_users"
+
+    telegram_id: int = Column(Integer, nullable=False, primary_key=True, unique=True)
+    user_id: int = Column(Integer, nullable=True, unique=True)
+    first_name: str = Column(String(2048), nullable=False)
+    last_name: str = Column(String(2048), nullable=True)
+    username: str = Column(String(2048), nullable=True)
+    created: _dt = Column(DateTime, server_default=func.now())
+    modified: _dt = Column(DateTime, server_onupdate=FetchedValue(), server_default=func.now(), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return f"TelegramUser(telegram_id={self.telegram_id}, user_id={self.user_id}, first_name={self.first_name})"
+
+
+class SharedMessage(Base):
+    """
+    Model representing a shared message between multiple unique Telegram chats
+    """
+
+    __tablename__ = "shared_messages"
+
+    id: int = Column(Integer, nullable=False, primary_key=True, autoincrement=True, unique=True)
+    share_type: str = Column(String(255), nullable=False)
+    share_id: int = Column(Integer, nullable=False)
+    chat_id: int = Column(Integer, nullable=False)
+    message_id: int = Column(Integer, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"SharedMessage(id={self.id}, share_type={self.share_type}, share_id={self.share_id})"

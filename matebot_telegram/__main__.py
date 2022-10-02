01b1e9d09ce2ca537db2e60bb15047c5b6e4e214
Chris <chris@hopfen.space>
2022-10-02 22:12:51 +0200

    Changed the bot's main entrypoint to use the new setup function exclusively

--- matebot_telegram/__main__.py
+++ matebot_telegram/__main__.py
@@ -1,56 +1,11 @@
 #!/usr/bin/env python3
 
-import typing
 import argparse
 import logging.config
 
-from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler, InlineQueryHandler
 from matebot_sdk.exceptions import APIConnectionException
 
-from matebot_telegram import api_callback, client, commands, config, registry, updater, util
-from matebot_telegram.commands.handler import FilteredChosenInlineResultHandler
-
-
-handler_types = typing.Union[
-    typing.Type[CommandHandler],
-    typing.Type[CallbackQueryHandler],
-    typing.Type[InlineQueryHandler],
-    typing.Type[FilteredChosenInlineResultHandler]
-]
-
-
-def _add(dispatcher: Dispatcher, handler: handler_types, pool: dict, pattern: bool = True) -> None:
-    """
-    Add the executors from the given pool to the dispatcher using the given handler type
-
-    :param dispatcher: Telegram's dispatcher to add the executor to
-    :type dispatcher: telegram.ext.Dispatcher
-    :param handler: type of the handler (subclass of ``telegram.ext.Handler``)
-    :type handler: handler_types
-    :param pool: collection of all executors for one handler type
-    :type pool: dict
-    :param pattern: switch whether the keys of the pool are patterns or names
-    :type pattern: bool
-    :return: None
-    """
-
-    logger.debug(f"Adding {handler.__name__} executors...")
-    for name in pool:
-        if pattern:
-            dispatcher.add_handler(handler(pool[name], pattern=name))
-        else:
-            dispatcher.add_handler(handler(name, pool[name], run_async=False))
-
-
-class NoDebugFilter(logging.Filter):
-    """
-    Logging filter that filters out any DEBUG message for the specified logger or handler
-    """
-
-    def filter(self, record: logging.LogRecord) -> int:
-        if super().filter(record):
-            return record.levelno > logging.DEBUG
-        return True
+from matebot_telegram import api_callback, client, commands, config, updater as _updater, util
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -59,7 +14,7 @@ def get_parser() -> argparse.ArgumentParser:
     return parser
 
 
-if __name__ == "__main__":
+def main():
     args = get_parser().parse_args()
     if args.config:
         config.setup_configuration(args.config)
@@ -68,7 +23,7 @@ if __name__ == "__main__":
     logger = logging.getLogger("root")
 
     logger.info("Registering bot token with Updater...")
-    updater = updater.PatchedUpdater(config.config.token, workers=config.config.workers)
+    updater = _updater.PatchedUpdater(config.config.token, workers=config.config.workers)
 
     logger.debug("Starting event thread...")
     util.event_thread.start()
@@ -82,22 +37,17 @@ if __name__ == "__main__":
     except APIConnectionException as exc:
         logger.critical(
             f"Connecting to the API server failed! Please review your "
-            f"config and ensure {config.config.server} is reachable."
+            f"config and ensure {config.config.server} is reachable: {exc!s}"
         )
         updater.callback_server and updater.callback_server.stop()
         util.event_thread_running.set()
         raise
 
-    logger.debug("Setting up commands...")
-    commands.setup()
+    logger.debug("Setting up feature handlers (e.g. commands, queries, messages, callbacks) ...")
+    commands.setup(updater.dispatcher)
 
     logger.debug("Adding error handler...")
     updater.dispatcher.add_error_handler(util.log_error)
-
-    _add(updater.dispatcher, CommandHandler, registry.commands, False)
-    _add(updater.dispatcher, CallbackQueryHandler, registry.callback_queries, True)
-    _add(updater.dispatcher, InlineQueryHandler, registry.inline_queries, True)
-    _add(updater.dispatcher, FilteredChosenInlineResultHandler, registry.inline_results, True)
 
     logger.info("Starting API callback server...")
     updater.start_api_callback_server()
@@ -105,3 +55,7 @@ if __name__ == "__main__":
     logger.info("Starting bot...")
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == "__main__":
+    main()

df7b8aeeb2dfe24feeb91c76ca74c90c256d09a8
Chris <chris@hopfen.space>
2023-01-23 03:56:34 +0100

    Added the three DB models required to serialize the arbitrary callback data

--- matebot_telegram/models.py
+++ matebot_telegram/models.py
@@ -2,10 +2,11 @@
 MateBot telegram persistence library
 """
 
+from typing import List
 from datetime import datetime as _dt
 
-from sqlalchemy import BigInteger, Column, DateTime, FetchedValue, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import BigInteger, Column, DateTime, FetchedValue, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
 
@@ -63,3 +64,51 @@ class RegistrationProcess(Base):
     core_user_id: int = Column(BigInteger, nullable=True)
     created: _dt = Column(DateTime, server_default=func.now())
     modified: _dt = Column(DateTime, server_onupdate=FetchedValue(), server_default=func.now(), onupdate=func.now())
+
+
+class CallbackButton(Base):
+    """
+    Model representing a single button for a inline keyboard markup
+    """
+
+    __tablename__ = "callback_buttons"
+
+    id: int = Column(Integer, nullable=False, primary_key=True, unique=True, autoincrement=True)
+    button_id: str = Column(String(32), nullable=False, unique=True)
+    """Example: '307b2b50d71f47ffa2edd1396e0acb57' or any other UUID string"""
+    button_text: str = Column(String(128), nullable=False)
+    """Example: 'donate confirm 100 42' or any other callback button string"""
+    keyboard_place: int = Column(Integer, nullable=False)
+    """Used for ordering of the entries to the keyboard buttons"""
+    keyboard_id: int = Column(Integer, ForeignKey("callback_keyboards.id", ondelete="CASCADE"), nullable=False)
+    """Reference to the keyboard containing the button"""
+
+    keyboard: "CallbackKeyboard" = relationship("CallbackKeyboard", back_populates="buttons")
+
+
+class CallbackKeyboard(Base):
+    """
+    Model representing an inline keyboard markup with attached callback data
+    """
+
+    __tablename__ = "callback_keyboards"
+
+    id: int = Column(Integer, nullable=False, primary_key=True, unique=True, autoincrement=True)
+    keyboard_id: str = Column(String(32), nullable=False, unique=True)
+    """Example: '121f23b5f42e496ca3996ea37296f6c5' or any other UUID string"""
+    creation_time: float = Column(Float, nullable=False)
+
+    buttons: List[CallbackButton] = relationship("CallbackButton", back_populates="keyboard", cascade="all,delete")
+
+
+class CallbackQuery(Base):
+    """
+    Model representing an active (or recently active) callback query
+    """
+
+    __tablename__ = "callback_queries"
+
+    id: int = Column(Integer, nullable=False, primary_key=True, unique=True, autoincrement=True)
+    query_id: int = Column(BigInteger, nullable=False)
+    keyboard_id: str = Column(String(32), nullable=False)  # no relation to CallbackKeyboard required by application
+    """Example: '8e8895b23d1b487e9167f256564b51a2' or any other UUID string"""
